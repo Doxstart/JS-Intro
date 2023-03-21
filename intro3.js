996c9e631bc7057d9d60c92757cd4fcbfaeaf790
Doxstart <127298109+Doxstart@users.noreply.github.com>
2023-03-21 13:34:35 +0100

    1ma lezione cicli

---
+++ intro3.js
@@ -0,0 +1,14 @@
+// alert('Ciao');
+// console.log(alert);
+
+// var x=10;
+// console.log("Stampa x:", x); //Stampa variabile
+
+// alert('a');
+// console.log(confirm('Sicuro?')); //funzione confirm
+
+// console.log(prompt('Digita il tuo nome')); //funzione prompt stampa contenuto in console
+
+// var myVariable = prompt('Digita il tuo nome'); //inserisco funzione prompt dentro una variabile
+// console.log(myVariable); //Stampare variabile dove ho definito funzione prompt
+
